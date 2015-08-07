5fa088565d6f1b5c70a6bd664a3b03685d3057ae
Bartek Wardzinski <bartosz.wardzinski@horus.com.pl>
2015-08-07 15:49:07 +0200

    new functions: reverse, filter, flatten

--- src/main/scala/sample/List.scala
+++ src/main/scala/sample/List.scala
@@ -12,6 +12,11 @@ case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List {
 
+  def apply[A](as: A*): List[A] = {
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+  }
+
   def tail[A](xs: List[A]) = xs match {
     case Nil => Nil
     case Cons(h, t) => t
@@ -55,4 +60,53 @@ object List {
     case Nil => z
     case Cons(h, t) => foldLeft(t, f(h, z))(f)
   }
+
+  def append[A](to: List[A], from: List[A]): List[A] = to match {
+    case Cons(h, t) => Cons(h, append(t, from))
+    case Nil => from
+  }
+
+  def appendFR[A](to: List[A], from: List[A]): List[A] = foldRight(to, from)((a,b) => Cons(a, b))
+
+  // Change order of `to` List
+  def appendFL[A](to: List[A], from: List[A]): List[A] = foldLeft(to, from)((a, b) => Cons(a, b))
+
+  def reverse[A](list: List[A]): List[A] = {
+    def helperFun[A](l: List[A], result: List[A]): List[A] = l match {
+      case Nil => result
+      case Cons(h, t) => helperFun(t, Cons(h, result))
+    }
+    helperFun(list, Nil: List[A])
+  }
+
+  def reverseFL[A](list: List[A]): List[A] = foldLeft(list, Nil: List[A])(Cons(_,_))
+
+  def flatten[A](lists: List[List[A]]): List[A] = foldRight(lists, Nil: List[A])((a, b) => append(a,b))
+
+  def addOneToList(list: List[Int]): List[Int] = list match {
+    case Nil => Nil
+    case Cons(h, t) => Cons(h+1, addOneToList(t))
+  }
+
+  def addOneToListFR(list: List[Int]): List[Int] = foldRight(list, Nil: List[Int])((h, t) => Cons(h+1, t))
+
+//  def addOneToListFL(list: List[Int]): List[Int] = {
+//    def helperFun(list: List[Int], result: List[Int]): List[Int] = list match {
+//      case Nil => result
+//      case Cons(h, t) => helperFun(t, Cons(h+1, result))
+//    }
+//    reverse(helperFun(list, Nil: List[Int]))
+//  }
+
+  def addOneToListFL(list: List[Int]): List[Int] = foldLeft(foldLeft(list, Nil: List[Int])((h, t) => Cons(h+1, t)), Nil: List[Int])(Cons(_, _))
+
+  def map[A,B](as: List[A])(f: A => B): List[B] = foldRight(as, Nil: List[B])((h, t) => Cons(f(h), t))
+
+  def filter[A](as: List[A])(f: A => Boolean): List[A] = as match {
+    case Nil => Nil
+    case Cons(h, t) => if (f(h)) Cons(h, filter(t)(f)) else filter(t)(f)
+  }
+
+  //def filterFR[A](as: List[A])(f: A => Boolean): List[A] = foldRight()
+
 }
